843ec29615f97425f9fc11c849cba893c6233323
harikrishnakota1 <harikrishnakota1@gmail.com>
2020-04-27 10:52:56 -0400

    Initial commit

---
+++ src/main/java/com/example/villanova/StudentRegistartionApplication.java
@@ -0,0 +1,13 @@
+package com.example.villanova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistartionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistartionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/villanova/StudentRegistartionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistartionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
